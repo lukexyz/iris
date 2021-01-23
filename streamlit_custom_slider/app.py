9d6e5037b6284874b8e9aa35e1353419899b53c2
lukexyz <lukedwoods@gmail.com>
2021-01-23 09:22:51 +0000

    update react component conection

--- streamlit_custom_slider/app.py
+++ streamlit_custom_slider/app.py
@@ -1,4 +1,6 @@
-import streamlit.components.v1 as components  # Import Streamlit
+# Import the wrapper function from your package
+from streamlit_custom_slider import st_custom_slider
+import streamlit as st
 
-# Render the h1 block, contained in a frame of size 200x200.
-components.html("<html><body><h1>Hello, World</h1></body></html>", width=200, height=200)
+st.title("Testing Streamlit custom components")
+st_custom_slider()
